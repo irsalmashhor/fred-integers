ec4e46436ebae6721da72d89513d42ecb710232a
Irsal Mashhor <imashhor@signaldataco.com>
2014-06-13 03:48:45 -0400

    GET routes implementation

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [ring/ring-json "0.3.1"]
+                 [ring-json-response "0.2.0"]
+                 [org.clojure/data.json "0.2.4"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler fred-integers.handler/app}
   :profiles
--- src/fred_integers/handler.clj
+++ src/fred_integers/handler.clj
@@ -1,12 +1,40 @@
 (ns fred-integers.handler
-  (:use compojure.core)
+  (:use compojure.core
+        ring.util.response
+        [ring.middleware.json-response :only [wrap-json-response]]
+        [ring.middleware.params :only [wrap-params]]
+        [ring.middleware.json :only [wrap-json-params]])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.data.json :as json]))
+
+
+(defn average [xs]
+  (/ (reduce + xs) (count xs)))
+
+(defn map-overall-average [m]
+  (if m
+    (average (map average (vals m)))
+    nil))
+
+(defn map-key-average [k m]
+  (average (m k)))
+
+(defn map-overall-average-handler [m] 
+  (response {:average (map-overall-average m)} ))
+
+(defn map-key-average-handler [k m] 
+  (response {:average (map-key-average k m)} ))
+
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/map-average" [m] (map-overall-average-handler (json/read-str m)))
+  (GET "/map-average/:k" [k m]  (map-key-average-handler k (json/read-str m)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (wrap-json-response
+  (wrap-params
+  (handler/site app-routes))))
