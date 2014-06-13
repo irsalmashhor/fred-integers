04b56ebff2b58415acec608788a5aca865356d16
Irsal Mashhor <imashhor@signaldataco.com>
2014-06-13 00:48:49 -0400

    Initial compojure template

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject fred-integers "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler fred-integers.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
